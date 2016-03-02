347e267992a40ae919e170a5cf9269ee725ccf0b
pravinbliss <pravin.blisslogix@gmail.com>
2016-03-02 15:10:58 +0530

    Added files via upload

---
+++ Birthdays_Final/Birthdays/PeopleListDataProviderProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  PeopleListDataProviderProtocol.swift
+//  Birthdays
+//
+//  Created by dasdom on 27.03.15.
+//  Copyright (c) 2015 Dominik Hauser. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import AddressBookUI
+
+public protocol PeopleListDataProviderProtocol: UITableViewDataSource {
+  var managedObjectContext: NSManagedObjectContext? { get set }
+  weak var tableView: UITableView! { get set }
+  
+  func addPerson(personInfo: PersonInfo)
+//  func personForIndexPath(indexPath: NSIndexPath) -> Person?
+  func fetch()
+}
